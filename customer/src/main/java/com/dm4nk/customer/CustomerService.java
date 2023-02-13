10038a80a4bf4975235c18bd12e97fd7df950263
dm4nk <dim.xx2011@yandex.ru>
2023-02-13 23:22:58 +0400

    Initial commit

---
+++ customer/src/main/java/com/dm4nk/customer/CustomerService.java
@@ -0,0 +1,17 @@
+package com.dm4nk.customer;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public record CustomerService(
+
+) {
+    public void registerCustomer(CustomerRegistrationRequest customerRegistrationRequest) {
+        Customer customer = Customer.builder()
+                .firstName(customerRegistrationRequest.firstName())
+                .lastName(customerRegistrationRequest.lastName())
+                .email(customerRegistrationRequest.email())
+                .build();
+        //todo dmpr add validation
+    }
+}
