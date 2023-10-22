2c04b10af890411fcfd4750577ef5fc351c360b2
dm4nk <dim.xx2011@yandex.ru>
2023-10-22 22:53:11 +0400

    added k8s configs for services, done with k8s configuration

--- clients/src/main/java/com/dm4nk/clients/notification/NotificationClient.java
+++ clients/src/main/java/com/dm4nk/clients/notification/NotificationClient.java
@@ -4,7 +4,10 @@ package com.dm4nk.clients.notification;
 import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.PostMapping;
 
-@FeignClient("notification")
+@FeignClient(
+        name="notification",
+        url = "${clients.notification.url}"
+)
 public interface NotificationClient {
     @PostMapping("api/v1/notification")
     void sendNotification(NotificationRequest notificationRequest);
