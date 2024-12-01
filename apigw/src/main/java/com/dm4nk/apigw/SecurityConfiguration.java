d39350ce18e1a58e0fb96ed124bce88b483ee6e7
Прокопович Дмитрий <prokopovich.di@esphere.ru>
2024-12-02 03:57:34 +0400

    authorization with keycloak and frontend

---
+++ apigw/src/main/java/com/dm4nk/apigw/SecurityConfiguration.java
@@ -0,0 +1,27 @@
+package com.dm4nk.apigw;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@Configuration
+@EnableWebFluxSecurity
+public class SecurityConfiguration {
+
+    @Bean
+    public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        return http
+                .csrf(ServerHttpSecurity.CsrfSpec::disable)
+                .authorizeExchange(exchanges -> exchanges
+                        .pathMatchers(HttpMethod.OPTIONS).permitAll()
+                        .pathMatchers("/bookshop.html", "/auth/**").permitAll()
+                        .anyExchange().authenticated()
+                )
+                .oauth2ResourceServer(jwt -> jwt.jwt(Customizer.withDefaults()))
+                .build();
+    }
+}
